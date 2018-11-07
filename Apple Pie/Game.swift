210ce7895cf0c2e922c1b80701cf87e2d54ac318
doortje <doortje@doortjes-MacBook-Pro.local>
2018-11-07 20:38:10 +0100

    Uploaden app

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by doortje on 07/11/2018.
+//  Copyright © 2018 Doortje. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+} 
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -1,20 +1,89 @@
-//
 //  ViewController.swift
 //  Apple Pie
-//
 //  Created by doortje on 07/11/2018.
 //  Copyright © 2018 Doortje. All rights reserved.
-//
 
 import UIKit
 
+// Class ViewController: Geeft basisfuncties van de game aan. Zoals welke woorden er gebruikt worden, hoeveel moves je mag maken en houdt de score bij. Daarnaast linkt hij alle views aan code.
 class ViewController: UIViewController {
+    
+    var listOfWords = ["varken", "hondenbrok", "potlood", "doekje", "patat", "voetbal", "rok", "computer", "bitcoin", "marktkantine", "paradiso", "antal"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
 
+    //Door ! geef je aan dat alle buttons een titel hebben en je lowercased alles zodat het makkelijker is om te vergelijken met elkaar.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+// viewDidLoad start een nieuwe ronde.
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+// newRound geeft aan hoe een nieuwe ronde in zijn werk gaat.
+    var currentGame: Game!
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord,
+            incorrectMovesRemaining: incorrectMovesAllowed,
+            guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
 }
-
+    func enableLetterButtons(_ enable: Bool){
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
+// Update het scorelabel en het plaatje van de boom. Update dus de interface van de app.
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+}
