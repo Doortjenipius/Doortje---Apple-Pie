9897eda35b40f9fa2815a9828466601765a4aca6
doortje <doortje@doortjes-MacBook-Pro.local>
2018-12-11 16:16:22 +0100

    Laatste versie

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -7,17 +7,22 @@
 //
 
 import Foundation
-// Code van de game, hoe het spel werkt. 
+// Code van de game, hoe het spel werkt.
+
+// Wanneer een letter niet in het woord zit, gaat er een appel van de boom.
 struct Game {
     var word: String
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
     mutating func playerGuessed(letter: Character) {
         guessedLetters.append(letter)
+        // als een letters niet in het woord zit krijgt incorrectMovesRemaing -1. 
         if !word.contains(letter) {
             incorrectMovesRemaining -= 1
         }
     }
+    
+    // guessedword wordt aangevuld met de juiste letters.
     var formattedWord: String {
         var guessedWord = ""
         for letter in word {
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Apple Pie Game 
 
-De Apple pie app is een game die je woorden laat raden. Het werkt als een soort galgje. Je probeert de woorden te raden door het aanklikken van de juiste letters. De app houdt zelf bij hoe vaak je verloren en gewonnen hebt. 
+De Apple pie app is een game die je woorden laat raden. Het werkt als een soort galgje. Je kiest voordat je begint de moeilijkheidsgraad van de woorde en vervolgens probeer je het woord te raden door het aanklikken van de juiste letters. De app houdt zelf bij hoe vaak je verloren en gewonnen hebt. 
 
-![screenshot](Doc/Apple_tree1.png)
-![screenshot](Doc/Apple_tree2.png)
+![screenshot](Doc/appleintro.png)
+![screenshot](Doc/apple_game1.png)
 
