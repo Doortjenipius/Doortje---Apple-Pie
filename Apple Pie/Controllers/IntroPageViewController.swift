14bb8f21b997ff8fb188b057680b94dba3683d07
doortje <doortje@doortjes-MacBook-Pro.local>
2018-12-17 19:43:56 +0100

    Laatste versie

--- Apple Pie/Controllers/IntroPageViewController.swift
+++ Apple Pie/Controllers/IntroPageViewController.swift
@@ -12,30 +12,30 @@ class IntroPageViewController: UIViewController {
     
     @IBAction func unwindToIntro(segue: UIStoryboardSegue){
     }
-
-// Lijst met woorden als string. 
+    
+    // Lijst met woorden als string.
     var listOfWords: [String]!
     
-// Button 'simple'. Als er op de button wordt gedrukt wordt het spel gestart met de woorden die in de lijst listOfWords staan. Deze worden doorgegevn doormiddel van de Segue.
+    // Button 'simple'. Als er op de button wordt gedrukt wordt het spel gestart met de woorden die in de lijst listOfWords staan. Deze worden doorgegevn doormiddel van de Segue.
     @IBAction func simple(_ sender: Any) {
         listOfWords = ["dog", "farm", "apple", "skirt", "goat", "nice","mom"]
         performSegue(withIdentifier: "applepage", sender: sender)
     }
     
-// Button 'medium'. Als er op de button wordt gedrukt wordt het spel gestart met de woorden die in de lijst listOfWords staan. Deze worden doorgegevn doormiddel van de Segue.
+    // Button 'medium'. Als er op de button wordt gedrukt wordt het spel gestart met de woorden die in de lijst listOfWords staan. Deze worden doorgegevn doormiddel van de Segue.
     @IBAction func medium(_ sender: Any) {
         listOfWords = ["table", "story", "soccer", "chairs", "picnic", "spider", "bagpack","milkshake"]
         performSegue(withIdentifier: "applepage", sender: sender)
     }
     
-// Button 'hard'. Als er op de button wordt gedrukt wordt het spel gestart met de woorden die in de lijst listOfWords staan. Deze worden doorgegevn doormiddel van de Segue.
+    // Button 'hard'. Als er op de button wordt gedrukt wordt het spel gestart met de woorden die in de lijst listOfWords staan. Deze worden doorgegevn doormiddel van de Segue.
     @IBAction func hard(_ sender: Any) {
         listOfWords = ["bitcoin", "applepie", "bookstore", "magazine", "pineapples", "apartment", "hamburger"]
         performSegue(withIdentifier: "applepage", sender: sender)
     }
     
     
-// de correcte listOfWords wordt doorgegeven naar ViewController.
+    // de correcte listOfWords wordt doorgegeven naar ViewController.
     override func prepare(for segue: UIStoryboardSegue, sender:
         Any?) {
         if segue.identifier == "applepage" {
@@ -46,23 +46,23 @@ class IntroPageViewController: UIViewController {
     }
     
     
-// Outlets van de 3 buttons zodat deze kunnen worden ge-edit.
+    // Outlets van de 3 buttons zodat deze kunnen worden ge-edit.
     @IBOutlet weak var simple_button: UIButton!
     @IBOutlet weak var middle_button: UIButton!
     @IBOutlet weak var hard_button: UIButton!
     
-// Outlet van het Label zodat deze kan worden ge-edit.
+    // Outlet van het Label zodat deze kan worden ge-edit.
     @IBOutlet weak var play: UILabel!
     
-// View wordt geladen met de juiste gegevens voor de interface.
+    // View wordt geladen met de juiste gegevens voor de interface.
     override func viewDidLoad() {
         super.viewDidLoad()
-// Radius van de buttons
+        // Radius van de buttons
         simple_button.layer.cornerRadius = 8
         middle_button.layer.cornerRadius = 8
         hard_button.layer.cornerRadius = 8
-
-// Background image code
+        
+        // Background image code
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "apple.jpg")
         backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
@@ -70,15 +70,15 @@ class IntroPageViewController: UIViewController {
     }
     
 }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
 
--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 De Apple pie app is een game die je woorden laat raden. Het werkt als een soort galgje. Je kiest voordat je begint de moeilijkheidsgraad van de woorde en vervolgens probeer je het woord te raden door het aanklikken van de juiste letters. De app houdt zelf bij hoe vaak je verloren en gewonnen hebt. 
 
+#### Extra
+Er is een mogelijkheid om voordat de game begint zelf de moeilijkheidsgraad van het spel te kiezen, de woordenlijst voor elke optie is anders. 
+
 ![screenshot](Doc/appleintro.png)
 ![screenshot](Doc/Apple_tree2.png)
 
